34c7ab9117142bc449cec7ba7eab40b2b1de8f72
Haenu <2523053533@qq.com>
2023-06-18 21:26:38 +0800

    登录授权SpringSecurity

---
+++ DBbackend/src/main/java/com/dlnu/config/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.dlnu.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/api/auth/login")
+                .successHandler(this::onAuthenticationSuccess)
+                .and()
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
+
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write("成功");
+    }
+    }
+
+
+
