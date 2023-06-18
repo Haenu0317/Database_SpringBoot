f982a7a8dbd3101dd89ff39243e4ad70490194d6
Haenu <2523053533@qq.com>
2023-06-18 17:48:36 +0800

    init commit

---
+++ src/main/java/com/dlnu/DatabaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dlnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseSpringBootApplication.class, args);
+    }
+
+}
