bd15e4bc3dfe54aa0d0aaf6141c0cb45a2dfad7e
Haenu <2523053533@qq.com>
2023-06-20 09:57:04 +0800

    设置cookie

--- DBbackend/src/main/java/com/dlnu/config/SecurityConfiguration.java
+++ DBbackend/src/main/java/com/dlnu/config/SecurityConfiguration.java
@@ -17,10 +17,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
 
@@ -31,8 +34,12 @@ public class SecurityConfiguration {
     @Resource
     AuthorizeService authorizeService;
 
+    //将cookie存储
+    @Resource
+    DataSource dataSource;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,PersistentTokenRepository repository) throws Exception {
         return http
                 .authorizeRequests()
                 .anyRequest().authenticated()
@@ -45,6 +52,12 @@ public class SecurityConfiguration {
                 .logout()
                 .logoutUrl("/api/auth/logout")
                 .logoutSuccessHandler(this::onAuthenticationSuccess)
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenRepository(repository)
+                //设置时间
+                .tokenValiditySeconds(60 * 60 * 24 * 7)
                 .and()
                 .csrf()
                 .disable()
@@ -71,6 +84,15 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cors);
         return source;
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(){
+        //使用JDBC来实现
+        JdbcTokenRepositoryImpl jdbcTokenRepository =new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        jdbcTokenRepository.setCreateTableOnStartup(false);
+        return jdbcTokenRepository;
     }
 
     @Bean
